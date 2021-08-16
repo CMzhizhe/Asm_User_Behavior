1c7091aeb5ec0c273be387fd0bdf1221fcf3c8b4
一个冬季 <745570899@qq.com>
2021-08-16 10:15:45 +0800

    1、更新repo

--- plugin/src/main/java/com/gxx/collectionuserbehaviorlibrary/costtime/CostTime.java
+++ plugin/src/main/java/com/gxx/collectionuserbehaviorlibrary/costtime/CostTime.java
@@ -4,9 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 /**
- * @date 创建时间:2021/7/28 0028
- * @auther gaoxiaoxiong
- * @Descriptiion  方法进行耗时统计
+ * date 创建时间:2021/7/28 0028
+ * auther gaoxiaoxiong
+ * Descriptiion  方法进行耗时统计
  * https://juejin.cn/post/6844903466188406792#heading-4
  **/
 @Target(ElementType.METHOD)
